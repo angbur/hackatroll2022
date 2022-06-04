ef97b028abb5f6cf4de0f8c6eecb7b02255b9fec
angbur <41025473+angbur@users.noreply.github.com>
2022-06-04 17:51:21 +0200

    feat: add layout

--- src/pages/Newtab/Newtab.jsx
+++ src/pages/Newtab/Newtab.jsx
@@ -1,28 +1,54 @@
-import React from 'react';
+import { Avatar, Box, Button, Card, CardActions, CardMedia, List, ListItem, ListItemAvatar, ListItemButton, ListItemText, Typography } from '@mui/material';
+import React, { useEffect, useState } from 'react';
 import logo from '../../assets/img/logo.svg';
 import './Newtab.css';
 import './Newtab.scss';
 
 const Newtab = () => {
-  
-  chrome.runtime.onMessage
-    .addListener(function(message,sender,sendResponse) { 
-        addImagesToContainer(message);               
-        sendResponse("OK");
-    });
+  const [ imageList, setImageList ] = useState([]);
+  const [ status, setStatus ] = useState('idle');
 
-
-    
-    const addImagesToContainer = (urls) => {
-      document.write(JSON.stringify(urls));
-      console.log(JSON.stringify(urls))
+  useEffect(()=>{
+    const getImageList = (message,sender,sendResponse) =>{
+      setImageList(message);              
+      sendResponse("OK");
   }
 
+  chrome.runtime.onMessage.addListener(getImageList);
+
+  return (()=>{
+    chrome.runtime.onMessage.removeListener(getImageList);
+  })
+
+  },[])
+  
   
   return (
-    <div className="App">
-      {addImagesToContainer()}
-    </div>
+        <Box className={"App"} display={'grid'}  p={2} m={3}  sx={{height: '100vh', gridTemplateColumns: 'repeat(auto-fit, minmax(400px, 1fr))'}}>
+          
+            {imageList ? imageList.map((img,id) => (
+              <Card key={`item-${id}`}
+               sx={{
+                    height: '400px', 
+                    boxShadow: ' 0 0 5px 5px #F0F0F0', 
+                    padding: '2rem', 
+                    margin: '2rem'}}>
+                    <CardMedia>
+                        <Box
+                            component="img"
+                            key={`itemAvatar-${id}`} 
+                            src={img.toString()}
+                            style={{height: '200px'}}
+                            alt=""
+                          />
+                    </CardMedia>
+                 <CardActions style={{display: 'flex', justifyContent: 'center'}}>
+                   <Button variant={'contained'} sx={{margin: '2rem 0'}}>Analizuj</Button>
+                 </CardActions>
+              </Card>
+            )): null}
+      
+        </Box>
   );
 };
 
